1ca667d4fd9b7fade862afa683106c749a2fd56a
unknown <alan.perez536@outlook.es>
2019-05-08 16:44:15 -0300

    dando estilo con vue.js al html

--- JavaScript/task 4/app.js
+++ JavaScript/task 4/app.js
@@ -1,6 +1,6 @@
 /**
  * @Date:   2019-04-28T16:03:36-03:00
- * @Last modified time: 2019-04-30T17:53:29-03:00
+ * @Last modified time: 2019-05-08T16:28:44-03:00
  */
 var app = new Vue({
   el: '#app1',
@@ -11,6 +11,7 @@ var app = new Vue({
     filter1: [],
     filter2: [],
     select:"",
+    text:"",
     pathname:window.location.pathname,
   },
   methods: {
@@ -27,25 +28,26 @@ var app = new Vue({
         .catch(err => console.log(err))
     },
     cargaPag: function(){
-      
+
        if(this.pathname.includes('/senate.html')){
          this.url="https://api.propublica.org/congress/v1/115/senate/members.json";
-        
+         this.text ="First convened in 1789, the composition and powers of the Senate are established in Article One of the U.S. Constitution. Each state is represented by two senators, regardless of population, who serve staggered six-year terms. The Senate has several exclusive powers not granted to the House, including consenting to treaties as a precondition to their ratification and consenting to or confirming appointments of Cabinet secretaries, federal judges, other federal executive    officials, military officers, regulatory officials, ambassadors, and other federal uniformed officers, as well as trial of federal officials impeached by the House.";
         }
        else if(this.pathname.includes('/house.html')){
          this.url = "https://api.propublica.org/congress/v1/115/house/members.json";
+         this.text = "First convened in 1789, the composition and powers of the Senate are established in Article One of the U.S. Constitution. Each state is represented by two senators, regardless of population, who serve staggered six-year terms. The Senate has several exclusive powers not granted to the House, including consenting to treaties as a precondition to their ratification and consenting to or confirming appointments of Cabinet secretaries, federal judges, other federal executive officials, military officers, regulatory officials, ambassadors, and other federal uniformed officers, as well as trial of federal officials impeached by the House."
      }
 
     },
   },
     computed: {
     mostrarTabla: function() {
-       if(this.select != "all"){ 
-       return this.filter2 = this.senateData.filter ((dato) => (dato.party == this.filter1[0] || 
+       if(this.select != "all"){
+       return this.filter2 = this.senateData.filter ((dato) => (dato.party == this.filter1[0] ||
        dato.party == this.filter1[1] ||  dato.party == this.filter1[2]) && dato.state == this.select );
        }
        else{
-        return this.filter2 = this.senateData.filter ((dato) => (dato.party == this.filter1[0] || 
+        return this.filter2 = this.senateData.filter ((dato) => (dato.party == this.filter1[0] ||
           dato.party == this.filter1[1] ||  dato.party == this.filter1[2]) );
        }
    }
